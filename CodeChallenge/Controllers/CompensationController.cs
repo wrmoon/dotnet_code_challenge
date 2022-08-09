cd5cde16ad3fc94c6a4256a4d12a04b79a2d6852
Bill Moon <gitlab@coolmoons.com>
2022-08-09 11:51:30 -0400

    tests work - task2 should be done.... but
    work remains

---
+++ CodeChallenge/Controllers/CompensationController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodeChallenge.Services;
+using CodeChallenge.Models;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/compensation")]
+    public class CompensationController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+
+        public CompensationController(ILogger<EmployeeController> logger, ICompensationService compensationService)
+        {
+            _logger = logger;
+            _compensationService = compensationService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        {
+            _logger.LogDebug($"Received compensation create request for employee '{compensation.Id}'");
+
+            _compensationService.Create(compensation);
+
+            return CreatedAtRoute("getCompensationByEmployeeId", new { id = compensation.Id }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationByEmployeeId")]
+        public IActionResult GetCompensationByEmployeeId(String id)
+        {
+            _logger.LogDebug($"Received compensation get request for employee '{id}'");
+
+            var compensation = _compensationService.GetByEmployeeId(id);
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
+    }
+}
